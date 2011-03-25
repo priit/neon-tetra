b25e24b136fc5dfcf117243dc3e6c27e271f9f95
Priit Tamboom <priit@gitnation.com>
2011-03-26 00:44:08 +0200

    rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fish::Application.config.secret_token = 'd49df5a15df639bf5e713409539fb937e7e8e17ac32a655b6f78f1fa584bc41ac63fc88a228345c0a710a08e445ec5b4ad1cc4d58f1e9dd08f158e2c50a6c08d'
