5b65b1a6b33072e45e48279a602afb9980f9bc4e
tanel <tanel@fortumo.com>
2011-03-26 14:55:26 +0200

    Fix Species Size field

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,2 +1,9 @@
 RailsAdmin.authenticate_with {}
 RailsAdmin.config {|c| c.label_methods << :common_name}
+#RailsAdmin.config do |config|
+#  config.model Species do
+#    edit do
+#      field :size, :float
+#    end
+#  end
+#end
