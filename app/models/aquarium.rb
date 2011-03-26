cf11332d97bdffb54dd584a6d940a390d3a4d9b8
tanel <tanel@fortumo.com>
2011-03-26 20:25:12 +0200

    Add fishes

--- app/models/aquarium.rb
+++ app/models/aquarium.rb
@@ -1,11 +1,32 @@
 class Aquarium < ActiveRecord::Base
   has_many :memberships
-  has_many :species, :through => :memberships
   
-  def add(species, amount = nil)
+  has_many :species, :through => :memberships do
+    def << (*items)
+      super( items - proxy_owner.species )
+    end
   end
   
-  def remove(species, amount = nil)
+  def add(sp, amount = nil)
+    sp = Species.find_by_id(sp.to_i) unless sp.is_a?(Species)
+    if sp && (amount = amount || sp.min_group_size).to_i > 0
+      self.species << sp
+      m = memberships.find_by_species_id(sp.id)
+      m.update_attribute(:amount, m.amount + amount)
+    end
+  end
+  
+  def remove(sp, amount = nil)
+    sp = Species.find_by_id(sp.to_i) unless sp.is_a?(Species)
+    amount = amount.to_i
+    
+    if sp && (m = memberships.find_by_species_id(sp.id))
+      if amount.to_i < 1 || (m.amount - amount) < 1
+        m.destroy
+      else
+        m.update_attribute(:amount, m.amount - amount)
+      end
+    end
   end
   
   def families
@@ -18,10 +39,10 @@ class Aquarium < ActiveRecord::Base
   end
   
   def ph_range
-    (species.collect {|s| s.min_ph}.max ... species.collect {|s| s.max_ph}.min)
+    (species.collect {|s| s.min_ph}.max .. species.collect {|s| s.max_ph}.min)
   end
   
   def dt_range
-    (species.collect {|s| s.min_dt}.max ... species.collect {|s| s.max_dt}.min)
+    (species.collect {|s| s.min_dt}.max .. species.collect {|s| s.max_dt}.min)
   end
 end
