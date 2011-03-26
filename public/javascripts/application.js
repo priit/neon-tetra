74b6c15b85fd5733e3c1344f3221fb426c615708
Martin Kangur <martin@cariba.ee>
2011-03-26 14:56:37 +0200

    someting fishy

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// This file is automatically included by javascript_include_tag :defaul
+
+function moreInfo(dsa) {
+  
+}
+
 function nextStep(elem) {
   $("#"+elem).prev().hide('slide', { direction: 'left' }, 1000);
   $('#'+elem).show('slide', { direction: 'right' }, 1000);
