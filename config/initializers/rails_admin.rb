8bef273226002112b387db402bbd83eb6bcf53e0
Priit Tamboom <priit@gitnation.com>
2011-03-26 18:06:53 +0200

    fix admin missing fields

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -13,14 +13,16 @@ RailsAdmin.config do |config|
     field :size_descriptor
     field :agressivness
     field :activity
-    field :shield
-    field :endurance
+    #field :shield
+    #field :endurance
     field :plant_eater
     field :min_group_size
     field :min_temperature
     field :max_temperature
     field :min_ph
     field :max_ph
+    field :min_dh
+    field :max_dh
     field :tag_list
     field :photo
   end
