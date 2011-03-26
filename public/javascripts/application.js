e6813bc7ad82c6e2c1079885b9d2ce92f6f6aa9c
Martin Kangur <martin@cariba.ee>
2011-03-26 12:51:15 +0200

    javascript and layouts

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function nextStep(elem) {
+  $("#"+elem).prev().hide('slide', { direction: 'left' }, 1000);
+  $('#'+elem).show('slide', { direction: 'right' }, 1000);
+}
+
+function prevStep(elem) {
+  console.log(elem);
+  $("#"+elem).next().hide('slide', { direction: 'right' }, 1000);
+  $('#'+elem).show('slide', { direction: 'left' }, 1000);
+}
