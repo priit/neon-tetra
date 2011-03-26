743d72327074917ad9c71351a8e77564bf57b3a3
Priit Tamboom <priit@gitnation.com>
2011-03-26 10:52:48 +0200

    added rpx and please do db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110325234242) do
+ActiveRecord::Schema.define(:version => 20110326081415) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20110325234242) do
     t.string   "authentication_token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "rpx_identifier"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
